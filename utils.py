7d59414e83d84a35a4e60d789fd402698f06162a
tsole0 <locke.apogee@gmail.com>
2025-08-21 21:43:41 +0900

    add utility function to reformat database from csv file. removes unneeded dependency.

--- utils.py
+++ utils.py
@@ -9,7 +9,28 @@ def hash_existing_db():
     with app.app_context():
         user_list = User.query.all()
         for user in user_list:
-            if not user.password.startswith('argon2:'):
+            if not user.password.startswith('scrypt:'):
                 # Most likely not a hashed password. Update it.
-                user.password = generate_password_hash(user.password, method='argon2')
+                user.password = generate_password_hash(user.password, method='scrypt')
         db.session.commit()
+
+def db_frorm_csv(filename: str):
+    """
+    :param filename: name of file in same directory as script to format
+
+    Utility function for re-formatting user database information from a csv file.
+    NOTE: Does not clear the database.
+    """
+    import csv
+    with app.app_context():
+        with open(filename, 'r', encoding='utf-8') as rice:
+            reader = csv.reader(rice)
+            for row in reader:
+                username = row[1]
+                password = row[2]
+                email = row[3]
+                # Check if user already exists
+                if not User.query.filter_by(username=username).first():
+                    new_user = User(username=username, password=password, email=email)
+                    db.session.add(new_user)
+            db.session.commit()
